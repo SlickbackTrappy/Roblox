185e8db94489a0c25890a594f38d6eae11f65fd9
Trappy <122176962+SlickbackTrappy@users.noreply.github.com>
2023-01-11 16:23:04 -0500

    Update main.lua

--- API/main.lua
+++ API/main.lua
@@ -1,15 +1,56 @@
 local api = {}
 
-function api.getAllRealNums(...)
-  local nums = {...}
-  local newnums = {}
-  for i,v in pairs(nums) do
-    if not type(v) == "number" then
-      return warn("[API-ERROR] Cannot 'getAllRealNums' on a "..type(v).." ["..i.."]")
+function api.removeConnectionsFromInstance(v, otherEvents)
+  local otherEvents = otherEvents or {}
+  local instanceEvents = {"AncestryChanged","AttributeChanged","Changed","ChildAdded","ChildRemoved","DescendantAdded","DescendantRemoving","childAdded"}
+  for _,eventName in pairs(instanceEvents) do
+    for _,v in next,getconnections(v[eventName]) do
+      v:Disable()
     end
-    return v>0 and table.insert(newnums, v) or table.insert(newnums, v-v-v)
   end
-  return newnums
+  for _,eventName in pairs(otherEvents) do
+    for _,v in next,getconnections(v[eventName]) do
+      v:Disable()
+    end
+  end
+end
+
+function api.protect(v, parent)
+  api.removeConnectionsFromInstance(parent)
+  local mt = getrawmetatable(game)
+  local oldindex = mt.__index
+  local oldnc = mt.__namecall
+  setreadonly(mt, false)
+  mt.__index = newcclosure(function(Self, key)
+    if not checkcaller() then
+      if Self[key] == v then
+        return
+      end
+    end
+    return oldindex(Self, key)
+  end)
+  mt.__namecall = newcclosure(function(Self, ...)
+    local method = getnamecallmethod()
+      if not checkcaller() then
+        local isRemoteMethod = method:lower():find('fireserver') or method:lower():find('invokeserver')
+        if not isRemoteMethod then
+          if Self == v or Self[method](Self,...) == v then
+            return
+          end
+          if method:lower():find('getchildren') or method:lower():find('getdescendants') then
+            local newTbl = {}
+            for _,item in pairs(Self[method](Self,...)) do
+              if item ~= v then
+                table.insert(newTbl,item)
+              end
+            end
+          return newTbl
+        end
+      end
+    end
+    return oldnc(Self, ...)
+  end)
+  setreadonly(mt, true)
 end
 
 return api
